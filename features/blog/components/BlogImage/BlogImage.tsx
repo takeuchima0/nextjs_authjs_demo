614fb27465075121dfa00ef4da470660b767747d
takeuchima0 <miyabiii1210@gmail.com>
2024-11-01 03:15:38 +0900

    prettier: execute prettier format

--- features/blog/components/BlogImage/BlogImage.tsx
+++ features/blog/components/BlogImage/BlogImage.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const BlogImage = () => {
   return (
     <div>
-      <p>1. features/blog/components配下で定義したBlogImageのコンポーネントです。</p>
+      <p>
+        1. features/blog/components配下で定義したBlogImageのコンポーネントです。
+      </p>
     </div>
   );
 };
--- features/blog/components/BlogList/BlogList.tsx
+++ features/blog/components/BlogList/BlogList.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const BlogList = () => {
   return (
     <div>
-      <p>2. features/blog/components配下で定義したBlogListのコンポーネントです。</p>
+      <p>
+        2. features/blog/components配下で定義したBlogListのコンポーネントです。
+      </p>
     </div>
   );
 };
