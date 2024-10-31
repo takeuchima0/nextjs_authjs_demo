3ba98665988904c073210b5b633c6b4fec87212f
takeuchima0 <miyabiii1210@gmail.com>
2024-11-01 02:39:05 +0900

    eslint: return errors code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
 
 const AboutPage = () => {
-
-    const indentWarningCheck = 1000;
-
-  const singleQuotesWaringCheck = 'シングルクォーテーションを使用しています。';
-  const doubleQuotesWaringCheck = "ダブルクォーテーションを使用しています。";
-
-  const semiColonWaringCheck = "文末にセミコロンを設定しない状態で警告が出ることを確認しています。"
-
-  var usedVariables = "varで変数宣言しています。";
-
-  const snake_case = "スネークケースで変数定義しています";
-
   return (
     <div>
       このページはAboutページです。
-      <p>ESLintで設定したインデント数の警告が出力されることを確認しています。  {indentWarningCheck}</p>
-      <p>{singleQuotesWaringCheck}</p>
-      <p>{doubleQuotesWaringCheck}</p>
-      <p>{semiColonWaringCheck}</p>
-      <p>{usedVariables}</p>
-      <p>{snake_case}</p>
     </div>
   );
 };
