d74537187304155819d434deeb03eda2c60e63b2
takeuchima0 <miyabiii1210@gmail.com>
2024-11-01 01:11:23 +0900

    eslint: rules for quote check warn

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,10 +4,15 @@ const AboutPage = () => {
 
     const indentWarningCheck = 1000;
 
+  const singleQuotesWaringCheck = 'シングルクォーテーションを使用しています。';
+  const doubleQuotesWaringCheck = "ダブルクォーテーションを使用しています。";
+
   return (
     <div>
       このページはAboutページです。
       <p>ESLintで設定したインデント数の警告が出力されることを確認しています。  {indentWarningCheck}</p>
+      <p>{singleQuotesWaringCheck}</p>
+      <p>{doubleQuotesWaringCheck}</p>
     </div>
   )
 }
