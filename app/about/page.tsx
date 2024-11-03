a62654612a1646563235b4d0234b8cfcc942ac24
takeuchima0 <miyabiii1210@gmail.com>
2024-11-03 13:57:13 +0900

    husky: test 2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,9 @@ const AboutPage = () => {
   // const bbb = 'aaa'; // シングルクォーテーションに変わった
   // const ccc = 'hoge'; // 文末のセミコロンが指定された
 
-  // test
+  const xxx = 123; // インデントが2に変わった
+  const yyy = 'aaa'; // シングルクォーテーションに変わった
+  const zzz = 'hoge'; // 文末のセミコロンが指定された
 
   return <div>このページはAboutページです。</div>;
 };
