789c427250f29d7e2a20f8279e2bc4e8b672ab47
takeuchima0 <miyabiii1210@gmail.com>
2024-11-04 17:55:38 +0900

    fix: api response status

---
+++ app/lib/api/client.ts
@@ -0,0 +1,7 @@
+// app/api/client.ts
+// const headers = {
+//   'Content-Type': 'application/json',
+//   Accept: 'application/json',
+//   'API-Key': process.env.API_KEY || '',
+//   Authorization: `Bearer ${process.env.AUTHORIZATION_TOKEN || ''}`,
+// };
