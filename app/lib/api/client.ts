370798dd069b9f472c3eae564638b6530d46f9e8
takeuchima0 <miyabiii1210@gmail.com>
2024-11-04 19:35:59 +0900

    fix: get request for query parameters

--- app/lib/api/client.ts
+++ app/lib/api/client.ts
@@ -33,11 +33,52 @@ export const getRequestWithPathParameters = async <T>(
   path: string,
   pathParams: Record<string, string | number>,
 ): Promise<T> => {
+  // setup for path parameters
   Object.entries(pathParams).forEach(([key, value]) => {
     path = path.replace(`:${key}`, encodeURIComponent(value));
   });
 
   const url = `${API_HOST}${path}`;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: API_REQUEST_OPTIONS.headers,
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error Status: ${response.status}`);
+  }
+
+  return response.json() as Promise<T>;
+};
+
+/**
+ * クエリパラメータを使ってGETリクエストを送信する関数
+ *
+ * @param {string} path                                             - APIエンドポイントのパス。
+ * @param {Record<string, string | number | boolean>} [queryParams] - クエリパラメータを含むオブジェクト。キーはパラメータ名、値はその値です。
+ * @returns {Promise<T>}                                            - APIのレスポンスデータ。JSON形式のデータを返します。
+ * @throws {Error}                                                  - HTTPリクエストが成功しなかった場合にエラーをスローします。
+ */
+export const getRequestWithQueryParameters = async <T>(
+  path: string,
+  queryParams: Record<string, string | number | boolean>,
+): Promise<T> => {
+  // setup for query parameters
+  const queryString = queryParams
+    ? '?' +
+      new URLSearchParams(
+        Object.entries(queryParams).reduce(
+          (acc, [key, value]) => {
+            // 各値を文字列に変換
+            acc[key] = String(value);
+            return acc;
+          },
+          {} as Record<string, string>,
+        ),
+      ).toString()
+    : '';
+
+  const url = `${API_HOST}${path}${queryString}`;
   const response = await fetch(url, {
     method: 'GET',
     headers: API_REQUEST_OPTIONS.headers,
